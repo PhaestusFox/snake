b471a0af40377a9ca0f6e32a6c5b7830a6b51a7e
PhaestusFox <phaestusfox@gmail.com>
2025-10-26 19:42:28 +0800

    spawn snake head

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_systems(Startup, spawn_camera);
+    app.add_systems(Startup, spawn_snake);
     app.run();
 }
 
 /// Just spawn a 2d camera for now
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands.spawn(Sprite {
+        custom_size: Some(Vec2::new(10.0, 10.0)),
+        ..Default::default()
+    });
 }

7d22dec92d01db8e674a16f3c660f8aa47e9121f
PhaestusFox <phaestusfox@gmail.com>
2025-10-26 16:17:10 +0800

    add a camera

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ use bevy::prelude::*;
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, spawn_camera);
     app.run();
+}
+
+/// Just spawn a 2d camera for now
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
